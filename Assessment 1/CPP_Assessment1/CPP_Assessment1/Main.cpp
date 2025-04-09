5e73e989d96f958df97000a3ad9fb35c3baaa1dc
Adam K <s181395@students.aie.edu.au>
2025-04-09 19:03:25 +0930

    Added functions for test log - Append - ToUpper and lower- Find Character

--- Assessment 1/CPP_Assessment1/CPP_Assessment1/Main.cpp
+++ Assessment 1/CPP_Assessment1/CPP_Assessment1/Main.cpp
@@ -30,23 +30,76 @@ int main() // added alot of couts to and \n for easier read on the console
 	//	Test 2 EqualTo Successful
 	//	Test 3 Append Failed
 
+	//getdata no need to test
+
 	time_t rawtime;
 	time(&rawtime);
-	
+
 	file << std::endl << std::endl;
-	file << ctime(&rawtime) << std::endl << std::endl;
 
-
-
+	// Testing Sring Length
 	String Test0 = String("Test 0");// testing length of string
 
 	if (Test0.Length() == 6)
 	{
-		file << "Test 0 - Length" << "Successful";
+		file << "Test 0 - Length" << " = " << "Successful";
 	}
 	else
-		file << "Test 0 - Length" << "Fail"; 
+		file << "Test 0 - Length" << " = " << "Fail";
 
+	file << std::endl << std::endl;// Line break
+
+	// Testing Append Function
+	String Test1 = String("Cheese");
+	String string2 = String("Burger");
+	String Answer1 = String("CheeseBurger");
+
+	Test1.Append(string2);
+
+	if (Test1 == Answer1)
+	{
+		file << "Test 1 - Append" << " = " << "Successful";
+	}
+	else
+		file << "Test 1 - Append" << " = " << "Fail";
+
+	file << std::endl << std::endl;// Line break
+
+	//Testing ToLower Function
+	String Test2 = String("pickles");
+
+	if (Test2.ToLower() == "pickles")
+	{
+		file << "Test 2 - ToLower" << " = " << "Successful";
+	}
+	else
+		file << "Test 2 - ToLower" << " = " << "Fail";
+
+	file << std::endl << std::endl; // Line break
+
+	// Testing ToUpper Function
+	String Test3 = String("LETTUCE");
+
+	if (Test3.ToUpper() == "LETTUCE")
+	{
+		file << "Test 3 - ToUpper" << " = " << "Successful";
+	}
+	else
+		file << "Test 3 - ToUpper" << " = " << "Fail";
+
+	file << std::endl << std::endl;
+
+	//Testing to FindCharacter Function
+	String Test4 = String("Tomato");
+	if (Test4.FindCharacter('a') >= 0 &&
+		Test4.FindCharacter('p') < 0)
+	{
+		file << "Test 4 - FindCharacter" << " = " << "Successful";
+	}
+	else
+		file << "Test 4 - FindCharacter" << " = " << "Fail";
+
+	file << std::endl << std::endl;
 
 	file.close(); //close file
 
@@ -84,7 +137,7 @@ int main() // added alot of couts to and \n for easier read on the console
 	std::cout << "This is the Append function using the user word." << '\n' << std::endl;
 	TextB.Append(TextW);
 	TextB.WriteToConsole();
-	
+
 	std::cout << '\n' << std::endl;
 
 	std::cout << "This is the ToLower function." << '\n' << std::endl;
@@ -111,12 +164,12 @@ int main() // added alot of couts to and \n for easier read on the console
 	std::cout << '\n' << std::endl;
 
 	std::cout << "This is the Replace function L to Z" << '\n' << std::endl;
-	int positionReplace = TextW.Replace('L','Z');
+	int positionReplace = TextW.Replace('L', 'Z');
 	TextW.WriteToConsole();
 
 	std::cout << '\n' << std::endl;
-	
-	std::cout << "This uses the strcmp function testing if "<< TextA.getdata() << " and "<< TextW.getdata() << "  are are the same (0) = false, (1) = true" << '\n' << std::endl;
+
+	std::cout << "This uses the strcmp function testing if " << TextA.getdata() << " and " << TextW.getdata() << "  are are the same (0) = false, (1) = true" << '\n' << std::endl;
 	std::cout << "Is users word less than world " << '\n' << std::endl;
 	bool result1 = TextA == TextW;
 	std::cout << result1 << '\n' << std::endl;
@@ -133,15 +186,11 @@ int main() // added alot of couts to and \n for easier read on the console
 	std::cout << size1 << '\n' << std::endl;
 	std::cout << '\n' << std::endl;
 
-	
+
 
 	std::cout << '\n' << std::endl;
 
 
 	return 0;
-
-
-
-
 
 }
